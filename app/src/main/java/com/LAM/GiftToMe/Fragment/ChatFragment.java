3927c99e54f9e1200b964a506b7915b625450ee0
giulioaugello <giulioaugello@gmail.com>
2020-12-16 18:31:04 +0100

    update filter chat

--- app/src/main/java/com/LAM/GiftToMe/Fragment/ChatFragment.java
+++ app/src/main/java/com/LAM/GiftToMe/Fragment/ChatFragment.java
@@ -222,13 +222,11 @@ public class ChatFragment extends Fragment {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
 
-                // TODO Auto-generated method stub
             }
 
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                // TODO Auto-generated method stub
             }
 
             @Override
